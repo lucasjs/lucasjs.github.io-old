8b57dde1ac0ae66c77a6156ab0235e41c9e1ea36
lucasjs <lucas.tcjb@gmail.com>
2017-10-08 18:32:21 -0300

    ESLint and Style Lint #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,68 @@
 var gulp = require('gulp');
+var stylint = require('gulp-stylint');
 var stylus = require('gulp-stylus');
-var connect = require('gulp-connect');
 var imagemin = require('gulp-imagemin');
+const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
-
-gulp.task('stylus', function () {
-    return gulp.src('./src/styles/*.styl')
-        .pipe(stylus({
-            compress: true
-        }))
-        .pipe(gulp.dest('./build/css'))
-        .pipe(connect.reload())
-});
-
-gulp.task('babel', () =>
-    gulp.src('./src/js/*.js')
-    .pipe(babel({
-        presets: ['env']
-    }))
-    .pipe(gulp.dest('./build/js'))
-    .pipe(connect.reload())
-);
-
-
-gulp.task('imagemin', () =>
-    gulp.src('.img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/img'))
-    .pipe(connect.reload())
-);
+var connect = require('gulp-connect');
 
 gulp.task('html', function () {
-    gulp.src('./src/views/*.html')
-    .pipe(connect.reload())
+	gulp.src('./src/views/*.html')
+		.pipe(connect.reload())
 });
 
+gulp.task('stylint', function () {
+	return gulp.src('./src/styles/*.styl')
+		.pipe(stylint())
+		.pipe(stylint.reporter());
+});
+
+gulp.task('stylus', function () {
+	return gulp.src('./src/styles/*.styl')
+		.pipe(stylus({
+			compress: true
+		}))
+		.pipe(gulp.dest('./build/css'))
+		.pipe(connect.reload())
+});
+
+gulp.task('eslint', () =>
+	gulp.src('./src/js/*.js')
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(connect.reload())
+);
+
+gulp.task('babel', () =>
+	gulp.src('./src/js/*.js')
+	.pipe(babel({
+		presets: ['env']
+	}))
+	.pipe(gulp.dest('./build/js'))
+	.pipe(connect.reload())
+);
+
+gulp.task('imagemin', () =>
+	gulp.src('.img/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('./build/img'))
+	.pipe(connect.reload())
+);
+
 gulp.task('watch', function () {
-    gulp.watch(['./src/styles/*.styl'], ['stylus'])
-    gulp.watch(['./src/js/*.js'], ['babel'])
-    gulp.watch(['./src/img/*.*'], ['imagemin'])
-    gulp.watch('./src/views/*.html', ['html']);
+	gulp.watch('./src/views/*.html', ['html']);
+	gulp.watch(['./src/styles/*.styl'], ['stylint', 'stylus'])
+	gulp.watch(['./src/js/*.js'], ['eslint', 'babel'])	
+	gulp.watch(['./src/img/*.*'], ['imagemin'])	
 });
 
 gulp.task('connect', function () {
-    connect.server({
-        root: './src/views',
-        livereload: true,
-        port: 8000
-    });
+	connect.server({
+		root: './src/views',
+		livereload: true,
+		port: 8000
+	});
 });
 
-gulp.task('build', ['stylus', 'babel', 'imagemin', 'html']);
+gulp.task('build', ['html', 'stylint', 'stylus', 'imagemin', 'eslint', 'babel']);
 gulp.task('server', ['connect', 'watch']);
